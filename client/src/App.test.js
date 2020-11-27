3537480526b3ebada85904b12c047d828e42b397
SvLowman <SvenLohmann@gmx.de>
2020-11-27 10:04:08 +0100

    :bug: Make app.test work

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders greeting", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello World/i);
+  const linkElement = screen.getByText(/header/i);
   expect(linkElement).toBeInTheDocument();
 });
