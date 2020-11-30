5729cdaeb60d0e7aa580297e93874ece4ad718c7
SvLowman <SvenLohmann@gmx.de>
2020-11-30 14:09:05 +0100

    :construction: Add upload function to server.js

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
+const { cloudinary } = require("./lib/cloudinary");
 const express = require("express");
 const path = require("path");
-
 const app = express();
 const port = process.env.PORT || 3047;
+
+// For connection to Cloudinary
+app.use(express.static("public"));
+app.use(express.json({ limit: "50mb" }));
+
+app.post("/api/upload", async (request, response) => {
+  try {
+    const fileStr = request.body.data;
+    const uploadedResponse = await cloudinary.uploader.upload(fileStr, {
+      upload_preset: "upload",
+    });
+    console.log(uploadedResponse);
+  } catch (error) {
+    console.error(error);
+    response.status(500).send("Error 500");
+  }
+});
 
 // Serve any static files
 app.use(express.static(path.join(__dirname, "client/build")));
