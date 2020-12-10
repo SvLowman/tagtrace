7b675d406d98f3b77b14898389b6ed2a20d9e6b9
SvLowman <SvenLohmann@gmx.de>
2020-12-10 10:33:37 +0100

    :construction: Create tagging gallery (not working properly)

--- client/src/pages/TaggingPage.js
+++ client/src/pages/TaggingPage.js
@@ -1,21 +1,53 @@
 import React, { useState } from "react";
-import { Button } from "../components/Button";
+import { useEffect } from "react";
 import styled from "styled-components/macro";
+import { Button } from "../components/Button";
 import { addNewTag } from "../utils/api";
+import { getImageObj } from "../utils/api";
+import useAsync from "../utils/useAsync";
 
 const Display = styled.div`
   border: solid 1px lightgray;
+  display: flex;
+  justify-content: center;
+`;
+const ImageContainer = styled.div`
+  height: 800px;
+  width: 800px;
+  border: solid 1px lightgray;
+`;
+const ImageSlide = styled.div`
+  border: solid 1px lightgray;
+`;
+const Thumbnail = styled.img`
+  width: 10vw;
+  margin: 0 0.5rem;
 `;
 const TagForm = styled.form`
   border: solid 1px lightgray;
 `;
+const TagNotifier = styled.div`
+  border: solid 1px lightgray;
+`;
 
 const TaggingPage = () => {
-  const url =
-    "https://res.cloudinary.com/tagtrace/image/upload/v1606822076/TagTrace/wmcwfmhypvc06pnjtcpg.jpg";
-  const imgNr = "001";
   const userName = "sven";
+  const imgNr = "3708dab5d7ffaf9e7193a15e98ad3cff";
+
+  const { data: userData, loading, error, doFetch } = useAsync(() =>
+    getImageObj(userName)
+  );
+  useEffect(() => {
+    doFetch();
+  }, []);
+
+  // console.log(userData.images[userData.images.length - 1].url);
+  // const selectedImage = userData.images[userData.images.length - 1];
+  // console.log(selectedImage.url);
+  // console.log(selectedImage.tags);
+
   const [tagName, setTagName] = useState("");
+  // const [selectedImage, setSelectedImage] = useState(userData.images[userData.images.length - 1]);
 
   const handleTagNameChange = (event) => {
     setTagName(event.target.value);
@@ -38,9 +70,31 @@ const TaggingPage = () => {
           <h2>Das hier ist die Tagging-Seite 🤔</h2>
         </section>
         <Display>
-          <p>Hier soll ein ausgewähltes Bild stehen</p>
-          <img src={url} alt="testImage" />
+          <ImageContainer>
+            <img
+              src="https://res.cloudinary.com/tagtrace/image/upload/v1607532604/TagTrace/sfa2ekyqkrpfzex3lmv5.jpg"
+              alt=""
+            />
+            {/* <img src={userData.images[userData.images.length - 1].url} alt="" /> */}
+            {/* <img src={selectedImage.url} alt="" /> */}
+          </ImageContainer>
         </Display>
+        <ImageSlide>
+          {loading && <p>Loading...</p>}
+          {error && <p>{error.message}</p>}
+          {userData &&
+            userData.images.map((image) => (
+              <Thumbnail
+                // style={{
+                //   border: selectedImage === image ? "2px solid red" : "",
+                // }}
+                key={image.imgNr}
+                src={image.url}
+                alt="alt"
+                // onClick={() => setSelectedImage(image)}
+              />
+            ))}
+        </ImageSlide>
         <TagForm onSubmit={handleSubmit}>
           <input
             type="text"
@@ -50,6 +104,12 @@ const TaggingPage = () => {
           ></input>
           <Button label="Darf ich button?" type="submit" />
         </TagForm>
+        <TagNotifier>
+          <p>Diese Tags hat das Bild schon:</p>
+          {/* {selectedImage.tags.map((tag) => (
+          <p>{tag}</p>
+  ))} */}
+        </TagNotifier>
       </div>
     </>
   );
