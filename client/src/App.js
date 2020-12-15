6b88fe248260de81d499543350a534a9fb086471
SvLowman <SvenLohmann@gmx.de>
2020-12-15 17:42:59 +0100

    :art: Renew header, wrap page elements into page container in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,9 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import Page from "./pages/Page";
+// import Page from "./pages/Page";
+import PageContainer from "./components/PageContainer";
+import TopElement from "./components/TopElement";
 import UploadPage from "./pages/UploadPage";
 import TaggingPage from "./pages/TaggingPage";
 import GalleryPage from "./pages/GalleryPage";
@@ -15,20 +17,22 @@ function App() {
   return (
     <div className="App">
       <QueryClientProvider client={queryClient}>
-        <Page />
         <Router>
-          <Switch>
-            <Route exact path="/">
-              <UploadPage />
-            </Route>
-            <Route exact path="/tagging">
-              <TaggingPage />
-            </Route>
-            <Route exact path="/gallery">
-              <GalleryPage />
-            </Route>
-          </Switch>
-          <BottomNav />
+          <PageContainer>
+            <TopElement />
+            <Switch>
+              <Route exact path="/">
+                <UploadPage />
+              </Route>
+              <Route exact path="/tagging">
+                <TaggingPage />
+              </Route>
+              <Route exact path="/gallery">
+                <GalleryPage />
+              </Route>
+            </Switch>
+            <BottomNav />
+          </PageContainer>
         </Router>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
---
+++ client/src/components/PageContainer.js
@@ -0,0 +1,7 @@
+import styled from "styled-components/macro";
+
+const PageContainer = styled.div`
+  border: solid 1px lightgray;
+`;
+
+export default PageContainer;
