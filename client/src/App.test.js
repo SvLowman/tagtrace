748fbd29a5fad09e78fe257d7aa3112a65d19db4
SvLowman <SvenLohmann@gmx.de>
2021-01-10 21:08:30 +0100

    :white_check_mark: Make app.test work

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 test("renders greeting", () => {
-  render(<App />);
+  render(
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  );
   const linkElement = screen.getByText(/Trace/i);
   expect(linkElement).toBeInTheDocument();
 });
